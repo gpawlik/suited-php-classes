bb8a7adcd23f041c50b37fb8da65330895cad0c6
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-08-08 15:30:10 +0200

    lang

---
+++ lang/dummy.php
@@ -0,0 +1,11 @@
+<?php
+
+lang::translate('filter_autolinkext_help');
+lang::translate('filter_autolink_help');
+lang::translate('filter_geshiHl_help');
+lang::translate('filter_markdown_help');
+lang::translate('filter_markdownExt_help');
+lang::translate('filter_media_help');
+lang::translate('filter_phphl_help');
+lang::translate('filter_markdownNoFollow_help');
+lang::translate('filter_stacklink_help');
