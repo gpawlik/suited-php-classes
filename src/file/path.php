cabe6fc0c6650522096a825337eaa3c8ebacbd71
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-09-01 11:49:03 +0200

    better realpath

--- src/file/path.php
+++ src/file/path.php
@@ -76,30 +76,37 @@ class path {
      * @return string The resolved path, it might not exist.
      */
     public static function truepath($path) {
-        // whether $path is unix or not
-        $unipath = strlen($path) == 0 || $path{0} != '/';
-        // attempts to detect if path is relative in which case, add cwd
-        if (strpos($path, ':') === false && $unipath)
+        $isEmptyPath = (strlen($path) == 0);
+        $isRelativePath = ($path{0} != '/');
+        $isWindowsPath = !(strpos($path, ':') === false);
+
+        if (($isEmptyPath || $isRelativePath) && !$isWindowsPath){
             $path = getcwd() . DIRECTORY_SEPARATOR . $path;
+        }
         // resolve path parts (single dot, double dot and double delimiters)
         $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-        $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-        $absolutes = array();
-        foreach ($parts as $part) {
-            if ('.' == $part)
+        $pathParts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+        $absolutePathParts = array();
+        foreach ($pathParts as $part) {
+            if ($part == '.')
                 continue;
-            if ('..' == $part) {
-                array_pop($absolutes);
+
+            if ($part == '..') {
+                array_pop($absolutePathParts);
             } else {
-                $absolutes[] = $part;
+                $absolutePathParts[] = $part;
             }
         }
-        $path = implode(DIRECTORY_SEPARATOR, $absolutes);
+        $path = implode(DIRECTORY_SEPARATOR, $absolutePathParts);
+
         // resolve any symlinks
-        if (file_exists($path) && linkinfo($path) > 0)
+        if (file_exists($path) && linkinfo($path) > 0){
             $path = readlink($path);
+        }
+
         // put initial separator that could have been lost
-        $path = !$unipath ? '/' . $path : $path;
+        $path = (!$isWindowsPath ? '/' . $path : $path);
+
         return $path;
     }
 
