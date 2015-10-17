994b0ae96cc68b94da9a77053b8965e879986f94
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-10-17 13:04:44 +0200

    add phantom.onError as this seems to solve some problems

--- src/js/grab-dom.js
+++ src/js/grab-dom.js
@@ -12,6 +12,19 @@ page.onResourceTimeout = function(e) {
   phantom.exit(1);
 };
 
+// error handler
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
+
 function displayHelp () {
     console.log('Usage:');
     console.log(system.args[0] + ' \'http://twitter.com/search/javascript\'');
