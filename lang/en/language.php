aae234366c9a6639733751496a4f224f511c7aaf
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-11-24 12:35:44 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,28 +2,41 @@
 
 $LANG = array();
 
-// Translation of file vendor/diversen/simple-php-classes/src/imap.php
+// Translation of file vendor/diversen/simple-php-classes/lang/dummy.php
 
-$LANG['No title'] = 'No title';
-// Translation of file vendor/diversen/simple-php-classes/src/moduleloader.php
-
-$LANG['filter_'] = 'filter_';
-// Translation of file vendor/diversen/simple-php-classes/src/user/defaultProfile.php
-
-$LANG['(Admin) Edit profile'] = '(Admin) Edit profile';
-$LANG['Logout'] = 'Logout';
-// Translation of file vendor/diversen/simple-php-classes/src/html/helpers.php
-
-$LANG['Edit'] = 'Edit';
-$LANG['Delete'] = 'Delete';
-$LANG['View'] = 'View';
-$LANG['Submit'] = 'Submit';
+$LANG['filter_autolinkext_help'] = 'Links will be added automaticly, e.g.: http://google.com';
+$LANG['filter_autolink_help'] = 'Automatic replacement of URLs with links, e.g. http://os-cms.net becomes <a href="http://os-cms.net">http://os-cms.net</a>';
+$LANG['filter_geshiHl_help'] = 'Code can be highlighted. e.g. for PHP you will use <span class="notranslate">[hl:php]</span> your php code<span class="notranslate">[/hl:php]</span>';
+$LANG['filter_markdown_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
+$LANG['filter_markdownExt_help'] = 'Content added to this field will be filtered according to the extended markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can also use HTML.';
+$LANG['filter_media_help'] = 'You can insert direct youtube, vimeo, or soundcloud links. e.g. http://www.youtube.com/watch?v=xTGKzWDakK8 or http://vimeo.com/39289096, https://soundcloud.com/kaskade/kaskade-quadron-waste-love-ice';
+$LANG['filter_phphl_help'] = 'Highlighted PHP code using <span class="notranslate">[hl:php]</span> your php code<span class="notranslate">[/hl:php]</span>';
+$LANG['filter_markdownNoFollow_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
+$LANG['filter_stacklink_help'] = 'Automatic replacement of URLs with links, e.g. http://os-cms.net becomes <a href="http://os-cms.net">http://os-cms.net</a>';
+$LANG['filter_markdownPublic_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
 // Translation of file vendor/diversen/simple-php-classes/src/upload/blob.php
 
 $LANG['File does not exists'] = 'File does not exists';
 $LANG['File is too large.'] = 'File is too large.';
 $LANG['Max size is '] = 'Max size is ';
 $LANG['This Content type is not allowed'] = 'This Content type is not allowed';
+// Translation of file vendor/diversen/simple-php-classes/src/user/defaultProfile.php
+
+$LANG['(Admin) Edit profile'] = '(Admin) Edit profile';
+$LANG['Logout'] = 'Logout';
+// Translation of file vendor/diversen/simple-php-classes/src/html.php
+
+$LANG['File'] = 'File';
+$LANG['Max size'] = 'Max size';
+// Translation of file vendor/diversen/simple-php-classes/src/html/helpers.php
+
+$LANG['Edit'] = 'Edit';
+$LANG['Delete'] = 'Delete';
+$LANG['View'] = 'View';
+$LANG['Submit'] = 'Submit';
+// Translation of file vendor/diversen/simple-php-classes/src/imap.php
+
+$LANG['No title'] = 'No title';
 // Translation of file vendor/diversen/simple-php-classes/src/user.php
 
 $LANG['You can not access this page, because your account has been locked!'] = 'You can not access this page, because your account has been locked!';
@@ -42,19 +55,6 @@ $LANG['Mime type is not allowed. '] = 'Mime type is not allowed. ';
 $LANG['These mime types are allowed '] = 'These mime types are allowed ';
 $LANG['File is too large'] = 'File is too large';
 $LANG['Max size is '] = 'Max size is ';
-// Translation of file vendor/diversen/simple-php-classes/src/html.php
+// Translation of file vendor/diversen/simple-php-classes/src/moduleloader.php
 
-$LANG['File'] = 'File';
-$LANG['Max size'] = 'Max size';
-// Translation of file vendor/diversen/simple-php-classes/lang/dummy.php
-
-$LANG['filter_autolinkext_help'] = 'Links will be added automaticly, e.g.: http://google.com';
-$LANG['filter_autolink_help'] = 'Automatic replacement of URLs with links, e.g. http://os-cms.net becomes <a href="http://os-cms.net">http://os-cms.net</a>';
-$LANG['filter_geshiHl_help'] = 'Code can be highlighted. e.g. for PHP you will use <span class="notranslate">[hl:php]</span> your php code<span class="notranslate">[/hl:php]</span>';
-$LANG['filter_markdown_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
-$LANG['filter_markdownExt_help'] = 'Content added to this field will be filtered according to the extended markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can also use HTML.';
-$LANG['filter_media_help'] = 'You can insert direct youtube, vimeo, or soundcloud links. e.g. http://www.youtube.com/watch?v=xTGKzWDakK8 or http://vimeo.com/39289096, https://soundcloud.com/kaskade/kaskade-quadron-waste-love-ice';
-$LANG['filter_phphl_help'] = 'Highlighted PHP code using <span class="notranslate">[hl:php]</span> your php code<span class="notranslate">[/hl:php]</span>';
-$LANG['filter_markdownNoFollow_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
-$LANG['filter_stacklink_help'] = 'Automatic replacement of URLs with links, e.g. http://os-cms.net becomes <a href="http://os-cms.net">http://os-cms.net</a>';
-$LANG['filter_markdownPublic_help'] = 'Content added to this field will be filtered according to the markdown syntax. <a href ="http://daringfireball.net/projects/markdown/syntax">This link</a> gives a description of the syntax. You can not use HTML directly.';
+$LANG['filter_'] = 'filter_';
